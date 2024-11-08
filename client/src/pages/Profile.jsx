1afff7b29687b158c2c8d87f3cbd1c19957f3f46
rutubhanderi <avenger2004rutu@gmail.com>
2024-11-08 19:01:55 +0530

    complete home page

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
-import {app} from '../firebase';
+
 import {
   updateUserStart,
   updateUserSuccess,
